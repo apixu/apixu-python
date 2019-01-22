2f240a138de64ebdff5977bee32919b9c87398f8
Andrei Avram <avramandrei@ymail.com>
2019-01-22 23:04:54 +0200

    Fix tests.

--- apixu/tests/history_tests.py
+++ apixu/tests/history_tests.py
@@ -7,14 +7,14 @@ class HistoryTestCase(unittest.TestCase):
     def test_history_invalid_api_key(self):
         client = ApixuClient('INVALID_KEY')
         with self.assertRaises(ApixuException) as cm:
-            client.forecast()
+            client.history()
 
         self.assertEqual(cm.exception.code, 2006)
 
     def test_history_no_api_key(self):
         client = ApixuClient()
         with self.assertRaises(ApixuException) as cm:
-            client.forecast()
+            client.history()
 
         self.assertEqual(cm.exception.code, 1002)
 
--- apixu/tests/search_tests.py
+++ apixu/tests/search_tests.py
@@ -7,14 +7,14 @@ class SearchTestCase(unittest.TestCase):
     def test_search_invalid_api_key(self):
         client = ApixuClient('INVALID_KEY')
         with self.assertRaises(ApixuException) as cm:
-            client.forecast()
+            client.search()
 
         self.assertEqual(cm.exception.code, 2006)
 
     def test_search_no_api_key(self):
         client = ApixuClient()
         with self.assertRaises(ApixuException) as cm:
-            client.forecast()
+            client.search()
 
         self.assertEqual(cm.exception.code, 1002)
 
